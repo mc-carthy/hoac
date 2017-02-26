ac72ee841f9a4a661391630b16ba39fbd1722f24
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-26 11:58:01 +0000

    Create menu screen and begin implementing coin pickups

---
+++ Assets/scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public void GoToNextScene ()
+    {
+        SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1, LoadSceneMode.Single);
+    }
+
+    public void Quit ()
+    {
+        Application.Quit ();
+    }
+
+}
